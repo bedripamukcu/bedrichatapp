8037624fed77e7f0263f4244d2fd5f5f96689a39
bedripamukcu <niyazibedripamukcu@gmail.com>
2025-02-05 13:35:11 +0000

    login-signup-backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express= require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('./routes/auth.js', authRoutes);
+
+mongoose.connect('mongodb://localhost:27017/chatapp', {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+}).then(() => console.log('Mongodb connected'))
+.catch(err => console.error(err))
+
+app.listen(5000, ()=> console.log("Server running on port 5000"))
 
 const server = http.createServer(app);
 const io = new Server(server, {
